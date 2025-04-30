8959ad3a17c41366426a2a19812040dcb3c26648
daocreator <ziablovdmytro@gmail.com>
2025-04-30 20:58:01 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,30 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command
+from aiogram.utils.markdown import hbold
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
-
 
 @dp.message(CommandStart())
 async def start_handler(message: Message):
     await message.answer(f"Привет, {hbold(message.from_user.first_name)}! Я бот DAO LifeGame. Готов к запуску ☀️")
 
-
 @dp.message(Command("день"))
 async def day_handler(message: Message):
     await message.answer("🗓 Сегодняшняя дата по DAO-календарю: (здесь будет вставка позже)")
 
+async def main():
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
-    import asyncio
-
-    async def main():
-        await dp.start_polling(bot)
-
     asyncio.run(main())
