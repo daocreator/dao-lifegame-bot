c876f7c39ec38c688591cb1e695f52faca78c01a
daocreator <ziablovdmytro@gmail.com>
2025-04-30 21:14:04 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,5 @@
-import os
-import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.enums.parse_mode import ParseMode
-from aiogram.types import Message
-from aiogram.filters import CommandStart, Command
-from aiogram.utils.markdown import hbold
-from dotenv import load_dotenv
+/app/bot.py:15: DeprecationWarning: Passing `parse_mode`, `disable_web_page_preview` or `protect_content` to Bot initializer is deprecated. This arguments will be removed in 3.5.0 version
 
-load_dotenv()
+Use `default=DefaultBotProperties(...)` instead.
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
-
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
-dp = Dispatcher()
-
-@dp.message(CommandStart())
-async def start_handler(message: Message):
-    await message.answer(f"Привет, {hbold(message.from_user.first_name)}! Я бот DAO LifeGame. Готов к запуску ☀️")
-
-@dp.message(Command("день"))
-async def day_handler(message: Message):
-    await message.answer("🗓 Сегодняшняя дата по DAO-календарю: (здесь будет вставка позже)")
-
-async def main():
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+  bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
